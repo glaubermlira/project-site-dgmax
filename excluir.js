5054c35356048e0fc718b158379bc40deba50e94
Your Name <you@example.com>
2021-01-19 03:46:39 -0300

    adicionando arquivos

---
+++ excluir.js
@@ -0,0 +1,26 @@
+// for (i=0; i <= 10; i++){
+//   var cont = i;
+//   var result = 5 * cont;
+//   console.log(5 + "X" + i + " = " + result);
+// }
+
+var personas = [
+  {
+    name: "Joao",
+    city: "Recife",
+    Tel: "+55 81 99999-5555"
+  },
+  {
+    name: "Catatau",
+    city: "Jaboatao",
+    Tel: "+55 81 99999-5555"
+  },
+  {   
+    name: "Juba",
+    city: "Olinda",
+    Tel: "+55 81 99999-5555",
+  }
+]
+
+console.log(personas[2].city);
+  
